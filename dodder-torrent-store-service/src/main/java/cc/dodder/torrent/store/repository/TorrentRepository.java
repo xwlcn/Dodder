f0498932837b6805a7e96b43ed60b2f1269fc3f1
xwlcn <384320097@qq.com>
2021-04-27 01:46:44 +0800

    big update

--- dodder-torrent-store-service/src/main/java/cc/dodder/torrent/store/repository/TorrentRepository.java
+++
@@ -1,9 +0,0 @@
-package cc.dodder.torrent.store.repository;
-
-import cc.dodder.common.entity.Torrent;
-import cc.dodder.torrent.store.repository.customer.TorrentDao;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TorrentRepository extends MongoRepository<Torrent, String>, TorrentDao {
-}
